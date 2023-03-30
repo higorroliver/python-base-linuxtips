64b7306948383dcc4bd0fcf2075116b00fca5211
Higor Rodrigues <higorro.oliveira@gmail.com>
2023-03-30 13:48:16 -0300

    Add novo script, crianda uma tabuada

---
+++ tabuada.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""
+Imprime a tabuada do 1 ao 10.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Higor R."
+
+# base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numeros = list(range(1,11))
+
+print(numeros)
+
+
+#Iterable(percorríveis)
+
+for numero in numeros:
+    print("Tabuada do: {}".format(numero))
+    for multiplic in numeros:
+        print(numero * multiplic)
+    print("----------------")    

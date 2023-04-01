724a92a6e2a1aa6445531aad134f72a5f12ca765
Higor Rodrigues <higorro.oliveira@gmail.com>
2023-04-01 01:51:19 -0300

    Alterando algumas características da tabuada

--- tabuada.py
+++ tabuada.py
@@ -4,19 +4,20 @@
 Imprime a tabuada do 1 ao 10.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Higor R."
 
 # base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-numeros = list(range(1,11))
-
-print(numeros)
-
-
 #Iterable(percorríveis)
+numeros = list(range(1,11))
+print(numeros)
+print()
 
+#criando a tabuada
 for numero in numeros:
-    print("Tabuada do: {}".format(numero))
+    print("{:^18}".format(f"Tabuada do: {numero}"))
     for multiplic in numeros:
-        print(numero * multiplic)
-    print("-" * 12)     
+        resultado = numero * multiplic
+        print("{:^18}".format(f"{numero} x {multiplic} = {resultado}"))
+    print("#" * 18)
+print("\N{party popper}"*8)
